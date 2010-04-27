jsj2008/kdegames

commit 5c82e90c63e6983f7b29cec4b27f9d4bd3e1c169

HandBoard: remove and _add: rename parameter data to removeData/addData
git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/KDE/kdegames@1115550 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

kajongg/src/board.py
@@ -752,24 +752,24 @@ class HandBoard(Board):
         self.scene().field.game.checkSelectorTiles()
         return tile
 
-    def remove(self, data):
+    def remove(self, removeData):
         """return tile or meld to the selector board"""
         if not (self.focusTile and self.focusTile.hasFocus()):
             hadFocus = False
-        elif isinstance(data, Tile):
-            hadFocus = self.focusTile == data
+        elif isinstance(removeData, Tile):
+            hadFocus = self.focusTile == removeData
         else:
-            hadFocus = self.focusTile == data[0]
-        if isinstance(data, Tile) and data.isBonus():
-            self.__removeTile(data) # flower, season
+            hadFocus = self.focusTile == removeData[0]
+        if isinstance(removeData, Tile) and removeData.isBonus():
+            self.__removeTile(removeData) # flower, season
         else:
-            if not self.player.game.isScoringGame() and isinstance(data, Tile):
-                self.__removeTile(data)
+            if not self.player.game.isScoringGame() and isinstance(removeData, Tile):
+                self.__removeTile(removeData)
             else:
-                if isinstance(data, Tile):
-                    data = self.meldWithTile(data)
-                assert data
-                for tile in data.tiles:
+                if isinstance(removeData, Tile):
+                    removeData = self.meldWithTile(removeData)
+                assert removeData
+                for tile in removeData.tiles:
                     self.__removeTile(tile)
         self.placeTiles()
         if hadFocus:
@@ -787,28 +787,28 @@ class HandBoard(Board):
                 self.remove(tile)
         self.player.game.field.handSelectorChanged(self)
 
-    def _add(self, data, lowerHalf=None):
+    def _add(self, addData, lowerHalf=None):
         """get tile or meld from the selector board"""
-        if isinstance(data, Meld):
-            data.tiles = []
-            for pair in data.pairs:
-                data.tiles.append(self.__addTile(Tile(pair)))
+        if isinstance(addData, Meld):
+            addData.tiles = []
+            for pair in addData.pairs:
+                addData.tiles.append(self.__addTile(Tile(pair)))
             self.placeTiles()
             if self.player.game.isScoringGame():
-                for tile in data.tiles[1:]:
+                for tile in addData.tiles[1:]:
                     tile.focusable = False
             else:
-                for tile in data.tiles:
+                for tile in addData.tiles:
                     focusable = True
                     if lowerHalf is not None and lowerHalf == False:
                         focusable = False
                     if self.player != self.player.game.myself:
                         focusable = False
                     tile.focusable = focusable
-            if data.tiles[0].focusable:
-                self.focusTile = data.tiles[0]
+            if addData.tiles[0].focusable:
+                self.focusTile = addData.tiles[0]
         else:
-            tile = Tile(data) # flower, season
+            tile = Tile(addData) # flower, season
             self.__addTile(tile)
             self.placeTiles()
             if self.player.game.isScoringGame():
@@ -1050,7 +1050,7 @@ class HandBoard(Board):
             action = menu.exec_(menuPoint)
             if not action:
                 return None
-            idx = action.data().toInt()[0]
+            idx = action.addData().toInt()[0]
         if tile.board == self:
             meld.tiles = []
         return meldVariants[idx]

commit 1f26ef2bad5c2ca930126d7eb8053223f0173cd4

fix a regression from r1115550
git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/KDE/kdegames@1119480 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

kajongg/src/board.py
@@ -1049,7 +1049,7 @@ class HandBoard(Board):
             action = menu.exec_(menuPoint)
             if not action:
                 return None
-            idx = action.addData().toInt()[0]
+            idx = action.data().toInt()[0]
         if tile.board == self:
             meld.tiles = []
         return meldVariants[idx]

kajongg/src/pylintrc
@@ -57,7 +57,9 @@ load-plugins=
 # R0903 too few public methods
 # W0511 TODO
 # W0122 exec statement
-disable-msg=R0903,R0902,W0122
+# E0211 method has no argument
+# disable-msg=R0903,R0902,W0122
+disable-msg=R0903,E0211
 
 
 [REPORTS]
@@ -67,7 +69,7 @@ disable-msg=R0903,R0902,W0122
 output-format=text
 
 # Include message's id in output
-include-ids=no
+include-ids=yes
 
 # Put messages in a separate file for each module / package specified on the
 # command line instead of printing them on stdout. Reports (if any) will be
@@ -133,7 +135,7 @@ required-attributes=
 
 # Regular expression which should only match functions or classes name which do
 # not require a docstring
-no-docstring-rgx=__.*__
+no-docstring-rgx=(fget|fset|(__.*__))
 
 # Regular expression which should only match correct module names
 module-rgx=(([a-z_][a-z0-9_]*)|([A-Z][a-zA-Z0-9]+))$
@@ -185,7 +187,7 @@ bad-functions=map,filter,apply,input
 init-import=no
 
 # A regular expression matching names used for dummy variables (i.e. not used).
-dummy-variables-rgx=_|dummy
+dummy-variables-rgx=_|dummy|fget|fset
 
 # List of additional names supposed to be defined in builtins. Remember that
 # you should avoid to define new builtins when possible.