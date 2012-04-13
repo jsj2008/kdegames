jsj2008/kdegames

commit 049e4636555d8c21a641b4aa8c7422b6c8eb743e

HandContent: pass all hidden tiles in a single block starting with R
this makes the API of the scoring engine easier. If we want to pass
concealed melds as such, HandContent will now leave them untouched.

git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/KDE/kdegames@1288958 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

kajongg/src/scoringengine.py
@@ -31,8 +31,7 @@ from PyQt4.QtCore import QString
 
 from util import m18n, m18nc, english, logException # , logDebug
 from query import Query
-from meld import Meld, meldKey, Score, meldsContent, Pairs, \
-    REST, CONCEALED
+from meld import Meld, meldKey, Score, meldsContent, Pairs, CONCEALED
 
 import rulecode
 
@@ -561,6 +560,8 @@ class HandContent(object):
         # pylint: disable=R0902,R0914,R0912,R0915
         self.ruleset = ruleset
         self.string = string
+        if string.count('R') > 1:
+            raise Exception('string has more than on R part:%s'%string)
         self.robbedTile = robbedTile
         self.computedRules = computedRules or []
         self.won = False
@@ -679,7 +680,7 @@ class HandContent(object):
         # pylint says too many branches
         if not isinstance(tiles, list):
             tiles = list([tiles])
-        hidden = meldsContent(self.hiddenMelds)
+        hidden = 'R' + ''.join(x.joined for x in self.hiddenMelds)
         # exposed is a deep copy of declaredMelds. If lastMeld is given, it
         # must be first in the list.
         exposed = (Meld(x) for x in self.declaredMelds)
@@ -697,13 +698,13 @@ class HandContent(object):
                         del meld.pairs[meld.pairs.index(tile.lower())]
                         del exposed[idx]
                         meld.conceal()
-                        hidden += ' ' + meld.joined
+                        hidden += meld.joined
                         break
         for idx, meld in enumerate(exposed):
             if len(meld.pairs) < 3:
                 del exposed[idx]
                 meld.conceal()
-                hidden += ' ' + meld.joined
+                hidden += meld.joined
         mjStr = self.mjStr
         if self.lastTile in tiles:
             parts = mjStr.split()
@@ -942,19 +943,13 @@ class HandContent(object):
                     self.tiles = self.tiles.replace(pair, '', 1)
         splits = self.tiles.split()
         splits.extend(boni)
-        rest = []
+        rest = ''
         for split in splits:
-            if len(split) > 8:
-                rest.append(split)
-                continue
-            meld = Meld(split)
-            if split[0].islower() \
-                or meld.meldType != REST:
-                self.melds.append(meld)
+            if split[0] == 'R':
+                rest = split[1:]
             else:
-                rest.append(split)
+                self.melds.append(Meld(split))
         if rest:
-            rest = ''.join(rest)
             rest = ''.join(sorted([rest[x:x+2] for x in range(0, len(rest), 2)]))
             self.melds.extend(self.split(rest))
         self.melds = sorted(self.melds, key=meldKey)
@@ -968,29 +963,27 @@ class HandContent(object):
         if not tileName:
             return string
         parts = string.split()
-        lPart = mPart = None
-        candidates = []
-        for idx, part in enumerate(parts):
+        mPart = ''
+        rPart = 'R%s' % tileName
+        unchanged = []
+        for part in parts:
             if part[0] in 'SBCDW':
-                candidates.append(idx)
-            elif part[0] == 'L':
-                lPart = idx
+                rPart += part
+            elif part[0] == 'R':
+                rPart += part[1:]
             elif part[0].lower() == 'm':
-                mPart = idx
-        assert candidates, 'we have no concealed tiles in %s' % string
+                mPart = part
+            elif part[0] == 'L':
+                pass
+            else:
+                unchanged.append(part)
         # combine all parts about hidden tiles plus the new one to one part
         # because something like DrDrS8S9 plus S7 will have to be reordered
         # anyway
-        parts[candidates[0]] = ''.join(parts[x] for x in candidates)
-        parts[candidates[0]] += tileName
-        if lPart:
-            parts[lPart] = 'L%s' % tileName
-        else:
-            parts.append('L%s' % tileName)
-        if mPart:
-            parts[mPart] = parts[mPart].capitalize()
-        for others in candidates[1:]:
-            parts[others] = ''
+        parts = unchanged
+        parts.append(rPart)
+        parts.append('L%s' % tileName)
+        parts.append(mPart.capitalize())
         return ' '.join(parts)
 
     def __categorizeMelds(self):

kajongg/src/player.py
@@ -494,7 +494,7 @@ class Player(object):
         assert not (self.concealedMelds and self.concealedTileNames)
         assert not isinstance(self.lastTile, Tile)
         assert not isinstance(withTile, Tile)
-        melds = [''.join(self.concealedTileNames)]
+        melds = ['R' + ''.join(self.concealedTileNames)]
         if withTile:
             melds[0] += withTile
         melds.extend(x.joined for x in self.exposedMelds)
@@ -590,7 +590,7 @@ class Player(object):
         for player in self.others():
             visible += player.visibleTiles.count([tileName.capitalize()])
             visible += player.visibleTiles.count([tileName.lower()])
-        for pair in Pairs(hand.tiles.replace(' ', '')):
+        for pair in Pairs(hand.tiles.replace(' ', '').replace('R','')):
             if pair.lower() == tileName.lower():
                 visible += 1
         return 4 - visible

kajongg/src/playfield.py
@@ -989,7 +989,7 @@ class PlayField(KXmlGuiWindow):
         """compile hand info into a string as needed by the scoring engine"""
         if self.scoringDialog:
             return self.scoringDialog.computeLastTile()
-        return ''
+        return 'xx'
 
     def computeLastMeld(self):
         """compile hand info into a string as needed by the scoring engine"""

kajongg/src/scoring.py
@@ -888,7 +888,7 @@ class ScoringDialog(QWidget):
         idx = self.cbLastTile.currentIndex()
         if idx >= 0:
             return str(self.cbLastTile.itemData(idx).toString())
-        return ''
+        return 'xx'
 
     def closeEvent(self, event):
         """the user pressed ALT-F4"""

commit cac0d445c57d5e802642de456b11b989b918b4c4

fix a young regression from r1288958
git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/KDE/kdegames@1289172 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

kajongg/src/handboard.py
@@ -310,7 +310,7 @@ class HandBoard(Board):
             if self.player.concealedMelds:
                 newLowerMelds = sorted(self.player.concealedMelds)
             else:
-                tileStr = ''.join(self.player.concealedTileNames)
+                tileStr = 'R' + ''.join(self.player.concealedTileNames)
                 handStr = ' '.join([tileStr, self.player.mjString(), 'Lxx'])
                 content = HandContent.cached(self.player.game.ruleset, handStr)
                 newLowerMelds = list(Meld(x) for x in content.sortedMeldsContent.split())

kajongg/src/scoringengine.py
@@ -924,13 +924,12 @@ class HandContent(object):
         # no matter how the tiles are grouped make a single
         # meld for every bonus tile
         boni = []
-        if 'f' in self.tiles or 'y' in self.tiles: # optimize
-            # we need to remove spaces from the hand string first
-            # for building only pairs with length 2
-            for pair in Pairs(self.tiles.replace(' ', '')):
-                if pair[0] in 'fy':
-                    boni.append(pair)
-                    self.tiles = self.tiles.replace(pair, '', 1)
+        # we need to remove spaces from the hand string first
+        # for building only pairs with length 2
+        for pair in Pairs(self.tiles.replace(' ', '').replace('R', '')):
+            if pair[0] in 'fy':
+                boni.append(pair)
+                self.tiles = self.tiles.replace(pair, '', 1)
         splits = self.tiles.split()
         splits.extend(boni)
         rest = ''